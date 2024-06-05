ff05c7c06a377baf99e3278c9a94e7c5771836b7
清水政起 <shimizumasaki@shimizuseikinoMacBook-Air.local>
2024-06-05 15:15:07 +0900

    php練習問題①,②

--- php-practice_1.php
+++ php-practice_1.php
@@ -1,41 +1,130 @@
 <?php
 // Q1 変数と文字列
-
+$name = '清水';
+echo '私の名前は「' . $name . '」です。';
 
 // Q2 四則演算
-
+$sum = 5 * 4;
+echo $sum;
+echo "\n" . $sum /2;
 
 // Q3 日付操作
-
+echo date('現在時刻は、Y年m月d日 H時i分s秒です。');
 
 // Q4 条件分岐-1 if文
-
+$device = 'windows';
+if ($device == 'windows' || $device == 'mac') {
+    echo '使用OSは' . $device . 'です。';
+}
+else {
+    echo 'どちらでもありません。';
+}
 
 // Q5 条件分岐-2 三項演算子
-
+$age = 19;
+$message = ($age >= 20) ? '成人です。' : '未成人です。';
+echo $message;
 
 // Q6 配列
+$kantou = ['東京都', '神奈川県', '埼玉県', '栃木県', '千葉県', '群馬県', '茨城県'];
+echo $kantou[3] . 'と' . $kantou[4] . 'は関東地方の都道府県です。';
 
 
 // Q7 連想配列-1
-
+$capital = ['東京都' => '新宿区', '神奈川県' => '横浜市', '千葉県' => '千葉市', '埼玉県' => 'さいたま市', '栃木県' => '宇都宮市', '群馬県' => '前橋市', '茨城県' => '水戸市'];
+echo $capital['東京都'];
+echo "\n";
+echo $capital['神奈川県'];
+echo "\n";
+echo $capital['千葉県'];
+echo "\n";
+echo $capital['埼玉県'];
+echo "\n";
+echo $capital['栃木県'];
+echo "\n";
+echo $capital['群馬県'];
+echo "\n";
+echo $capital['茨城県'];
 
 // Q8 連想配列-2
-
+$capital = ['東京都' => '新宿区', '神奈川県' => '横浜市', '千葉県' => '千葉市', '埼玉県' => 'さいたま市',
+'栃木県' => '宇都宮市', '群馬県' => '前橋市', '茨城県' => '水戸市', '大阪府' => '大阪市', '静岡県' => '静岡市'];
+foreach ($capital as $key => $value) {
+    if ($key == '埼玉県') {
+        echo $key . 'の県庁所在地は' . $value . 'です。';
+    }
+}
 
 // Q9 連想配列-3
+foreach ($capital as $key => $value) {
+  if ($key == '大阪府' || $key == '静岡県') {
+      echo $key . 'は関東地方ではありません。';
+      echo "\n";
+  }
+  else {
+      echo $key . 'の県庁所在地は' . $value . 'です。';
+      echo "\n";
+  }
 
+  // if (in_array('大阪府', $capital)) {
+  //   echo $key . 'は関東地方ではありません。';
+  //   echo "\n";
+  // }
+  // else {
+  //     echo $key . 'の県庁所在地は' . $value . 'です。';
+  //     echo "\n";
+  // }
+}
 
 // Q10 関数-1
-
+function hello($name) {
+  echo $name . 'さん、こんにちは。';
+}
+hello('清水');
+echo "\n";
+hello('中村');
 
 // Q11 関数-2
-
+function calcTaxInPrice($price) {
+  $taxInPrice = $price * 1.1;
+  return $price . 'の商品の税込価格は' . $taxInPrice . 'です。';
+}
+echo calcTaxInPrice(1000);
 
 // Q12 関数とif文
-
+function distinguishNum($num) {
+  if ($num % 2 === 0) {
+      echo $num . 'は偶数です。';
+  }
+  else {
+      echo $num . 'は奇数です。';
+  }
+}
+distinguishNum(7);
+echo "\n";
+distinguishNum(12);
 
 // Q13 関数とswitch文
-
+function evaluateGrade($grade) {
+  switch ($grade) {
+      case 'A':
+      case 'B':
+          echo '合格です。';
+          break;
+          
+      case 'C':
+          echo '合格ですが追加課題があります。';
+          break;
+          
+      case 'D':
+          echo '不合格です。';
+          break;
+      
+      default:
+          echo '判定不明です。講師に問い合わせてください。';
+          break;
+  }
+}
+evaluateGrade('B');
 
 ?>
--- php-practice_2.php
+++ php-practice_2.php
@@ -1,15 +1,120 @@
 <?php
 // Q1 tic-tac問題
-
+echo '1から100までのカウントを開始します';
+ echo "\n";
+ echo "\n";
+for ($i = 1; $i <= 100; $i++) {
+    if ($i % 4 === 0 && $i % 5 === 0) {
+        echo 'tic-tac';
+        echo "\n";
+    }
+    elseif ( $i % 4 === 0 ) {
+        echo 'tic';
+        echo "\n";
+    }
+    elseif ($i % 5 === 0) {
+        echo 'tac';
+        echo "\n";
+    }
+    else {
+        echo $i;
+        echo "\n";
+    }
+}
+// for文で1から100をループで取得している。for文内にif,elseif,else文で条件分岐とその処理内容を書いている。
 
 // Q2 多次元連想配列
+$personalInfos = [
+  [
+      'name' => 'Aさん',
+      'mail' => 'aaa@mail.com',
+      'tel'  => '09011112222'
+  ],
+  [
+      'name' => 'Bさん',
+      'mail' => 'bbb@mail.com',
+      'tel'  => '08033334444'
+  ],
+  [
+      'name' => 'Cさん',
+      'mail' => 'ccc@mail.com',
+      'tel'  => '09055556666'
+  ],
+];
 
+echo $personalInfos[1]['name'] . 'の電話番号は' . $personalInfos[1]['tel'] . 'です。';
+// 変数のインデックス番号と指定したキー名を[]に入れることで、指定のバリューが取得できる。
 
-// Q3 オブジェクト-1
+foreach ($personalInfos as $x => $y) {
+    echo ($x+1) .'番目の' . $personalInfos[$x]['name'] . 'のメールアドレスは' . $personalInfos[$x]['mail'] . 'で、電話番号は'. $personalInfos[$x]['tel'] . 'です。';
+    // 上の問題と同様にしてバリューを取得。こちらはforeachで配列内要素をループ処理で取得している。
+    echo "\n"; // 初期値のキーが1に変更されていない
+}
 
+$ageList = [25, 30, 18];
+foreach ($ageList as $x => $y) {
+// キーが'age'、バリューが25,30,18
+$personalInfos[$x]['age'] = $y; // ここで0番目のageは25,1番目のageは30,2番目のageは30という処理をループで取得し、更に$personalInfosに追加している。
+}
+var_dump($personalInfos);
 
-// Q4 オブジェクト-2
+// Q3,4 オブジェクト-1,2
+class Student //クラス作成。クラスとはオブジェクトを構成する大きな枠組み。オブジェクトの設計図の役割を果たす。
+              // 関連のあるメソッドやプロパティをまとめることで一つのクラスとなる。
+{
+    public $studentId; // $studentIdプロパティを作成。publicはアクセス修飾子と呼ばれ、プロパティ、メソッドを呼び出すキーワード。
+    public $studentName; // $studentNameプロパティを作成。
 
+    public function __construct($id, $name) // コンストラクトメソッド作成。インスタンス化の際に自動で実行される
+    {
+        $this->studentId = $id; // 1つ目の引数を$studentIdプロパティにセット。$thisはインスタンス自身を表す。
+                                // $this->studentIdは$studentIdということらしい(別に$studentIdでいいやんと思ってしまう...)
+        $this->studentName = $name; // 2つ目の引数を$studentNameプロパティにセット
+    }
+
+    public function attend($className) // 変数classNameに呼び出し元の引数PHPが代入される。
+    {
+        echo $this->studentName . 'は' . $className . 'の授業に参加しました。学籍番号:' . $this->studentId ;
+        // クラス内ではインスタンス自身を表す$thisを使って変数を指定する
+    }
+}
+
+$yamada = new Student(120, '山田'); // インスタンス化（クラスを実体化したもの)。クラスを呼び出すことでインスタンス化できる。
+echo '学生番号' . $yamada->studentId . '番の生徒は' . $yamada->studentName . 'です。'
+echo "\n";
+$shimizu = new Student(6, '清水');
+$shimizu->attend('PHP'); // クラス内のattendメソッドを呼び出す
 
 // Q5 定義済みクラス
+$now = new DateTime(); // この1行だけでDateTimeクラスが呼び出せる
+echo $now->modify('-1 months')->format('Y-m-d');
+// formatメソッドで現在日時や曜日が取得できる。また文字列型で取得することもできる。
+// modifyメソッドは日時変更をしたい際に使用する
+
+$now = new DateTime(); // 変数nowに現在日時を代入
+$past = new DateTime('1992-04-25'); //  変数pastに指定日時を代入
+$diff = $now->diff($past); // diffメソッドを使って現在日時と指定日時の差分を取得し変数diffに代入
+echo 'あの日から' . $diff->format('%a') . '日経過しました。'; // formatメソッドを使って差分日数を取得して出力
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
